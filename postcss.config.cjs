db76b3103394c93fc96148619822b736f065b24f
Shashou2023 <122095432+Shashou2023@users.noreply.github.com>
2023-03-09 16:01:48 +0100

    Update postcss.config.cjs

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,6 +1,14 @@
 module.exports = {
   plugins: {
-    tailwindcss: {},
+    tailwindcss: {
+      content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],},
     autoprefixer: {},
   },
 }
