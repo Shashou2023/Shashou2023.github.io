0f8e2d42fddd3afc3555fe23544881f497ad4c53
Shashou2023 <122095432+Shashou2023@users.noreply.github.com>
2023-03-10 15:57:43 +0100

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -96,7 +96,7 @@ return(
     
     </div>
     <div className="relative" data-te-dropdown-ref>
-        <a href="#" target="_blank">   <button
+        <a href="#">   <button
       type="button"
       className="inline-block rounded-full bg-yellow-500 px-6 pt-2.5 pb-2 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#54b4d3] transition duration-150 ease-in-out hover:bg-info-600 hover:shadow-[0_8px_9px_-4px_rgba(84,180,211,0.3),0_4px_18px_0_rgba(84,180,211,0.2)] focus:bg-info-600 focus:shadow-[0_8px_9px_-4px_rgba(84,180,211,0.3),0_4px_18px_0_rgba(84,180,211,0.2)] focus:outline-none focus:ring-0 active:bg-info-700 active:shadow-[0_8px_9px_-4px_rgba(84,180,211,0.3),0_4px_18px_0_rgba(84,180,211,0.2)]">
       Prendre RDV
